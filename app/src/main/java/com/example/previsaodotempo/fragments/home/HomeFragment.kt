1c33afd775c72e6eff23e62773ebc61296d63e53
VitorAlvesDevv <vitor.alves.santos14@outlook.com>
2025-10-26 13:35:05 -0300

    Colocamos a função ao botao para localizar a localização do usuario, e as permissoes a localização do usuario

--- app/src/main/java/com/example/previsaodotempo/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/previsaodotempo/fragments/home/HomeFragment.kt
@@ -1,16 +1,24 @@
 package com.example.previsaodotempo.fragments.home
 
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.previsaodotempo.data.CurrentLocation
 import com.example.previsaodotempo.databinding.FragmentHomeBinding
+
 import java.text.SimpleDateFormat
 import java.util.Locale;
 import java.util.Date;
+import android.Manifest
+
+
 
 
 class HomeFragment : Fragment() {
@@ -19,10 +27,18 @@ class HomeFragment : Fragment() {
     private val binding get() = requireNotNull(_binding)
 
     private val PrevisaoMeteorologica = PrevisaoMeteorologica(
-        onLocationClicked = {
-            Toast.makeText(requireContext(),  "onLocationClicked()", Toast.LENGTH_SHORT).show()
-        }
+        onLocationClicked = {showLocationOptions() }
     )
+
+    private val locationPermissionLaucher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            getCurrentLocation()
+        } else {
+            Toast.makeText(requireContext(), "Permissão Negada", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,4 +67,43 @@ class HomeFragment : Fragment() {
         val formatter = SimpleDateFormat("d MMMM yyyy", Locale.getDefault())
         return "Hoje, ${formatter.format(currentDate)}"
     }
+
+    private fun getCurrentLocation() {
+        Toast.makeText(requireContext(), "getCurrentLocation()", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission (
+            requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        locationPermissionLaucher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private fun proceedWithCurrentLocation() {
+        if (isLocationPermissionGranted()) {
+            getCurrentLocation()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun showLocationOptions() {
+        val options = arrayOf("Localização atual", "Pesquisa Manual")
+        AlertDialog.Builder(requireContext()).apply {
+            setTittle("Selecione seu metodo de Localização")
+            setItems(options) { _, which ->
+                when (which) {
+                    0 -> proceedWithCurrentLocation()
+                }
+            }
+            show()
+        }
+    }
+
+    private fun setTittle(string: String) {}
+
+
 }
